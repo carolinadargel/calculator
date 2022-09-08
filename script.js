e5573f96b53047d9f896d4e6af9f4c05434d6d12
Your Name <carolinadargel@Carolinas-Mac-mini.local>
2022-09-08 17:27:16 -0300

    decimals and backspace working

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//main object to manage states
 let calculator = {
     displayValue: '',
     firstValue: null, 
@@ -6,12 +7,14 @@ let calculator = {
     subtotal: '',
     };
 
+//Destructuring object to access keys easily
 let v1 = calculator.firstValue;
 let v2 = calculator.SecondValue;
 let displayValue = calculator.displayValue;
 let operator = calculator.operator;
 let subtotal = calculator.subtotal;  
 
+//Variables needed to validate inputs
 let operatorList = ["+", "-", "*", "/"];
 let numberRegex = /^[0-9]+$/;
 
@@ -33,14 +36,17 @@ function multiply(n1, n2){
 
 //DIVIDE FUNCTION
 function divide(n1, n2){
+    if (n2 === 0) {
+        return 0;
+    }
     return n1 / n2;
 };
 
 //OPERATION FUNCTION
 function operate(operator, s1, s2){
 
-    let n1 = parseInt(s1);
-    let n2 = parseInt(s2);
+    let n1 = parseFloat(s1);
+    let n2 = parseFloat(s2);
     
     switch(operator) {
         case "+": 
@@ -63,36 +69,90 @@ function displayUpdate(){
 function display(value) {
 
     let currentNumber = document.getElementById("display").value;
-    
-    if (value.match(numberRegex)) {
-        if (!v1) {
-            displayValue = currentNumber + value;
-            displayUpdate();    
-        } else {
-        v2 += value;
-        displayValue = v2;
-        displayUpdate();
-        }
-    } else if (operatorList.includes(value) && !operator){
-        v1 = document.getElementById("display").value;
-        operator = value;
-        displayValue = '';
-        displayUpdate();
 
-    } else if (operatorList.includes(value) && operator){
-        v2 = document.getElementById("display").value;
-        subtotal = operate(operator, v1, v2);
-        displayValue = subtotal;
-        v1 = subtotal;
-        operator = value;
-        v2 = "";
-        displayUpdate();
+    document.getElementById("btn-clear").value = 'C'; // activate btn C over AC everytime a key is clicked
+
+    switch (value) {
+
+        case 'c':
+            clear();
+            break;
+
+        case '.':
+            inputDecimal(value);
+            break;
+            
+        case '=':
+            subtotal = operate(operator, v1, v2);
+            displayValue = subtotal;
+            break;
+               
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            if (operatorList.includes(value) && !operator){
+                v1 = document.getElementById("display").value;
+                operator = value;
+                displayValue = '';
+                v2 = "";
+
+
+            } else if (operatorList.includes(value) && operator){
+                v2 = document.getElementById("display").value;
+                subtotal = operate(operator, v1, v2);
+                displayValue = subtotal;
+                v1 = subtotal;
+                operator = value;
+                v2 = "";
+            }
+            break;
+        
+        default:
+            if (value === 'backspace'){
+                displayValue = displayValue.slice(0, -1);
+                v2 = displayValue;
+            } else{
+                v2 += value;
+                if (!v1) {
+                    displayValue = currentNumber + value;
+                } else {
+                    displayValue = v2;
+                }
+            }
+                break;
     }
+    displayUpdate();
 }
 
+function inputDecimal(dot) {
+    console.log('inside decimal function')
+    // if nothing is displayed as v2, add '0.'
+    if (v2 === '') {
+        displayValue = "0.";
+        displayUpdate();
+    }
+    if (!displayValue.includes(dot)) {
+        displayValue += ".";
+        v2 = displayValue;
+        displayUpdate();
+    }
+  }
 
-//CLEAR ALL THE VALUES
+//CLEAR DISPLAY
 function clear() {
-    displayValue = '0';
+
+    //if display is already clear, reset history
+    if(displayValue == null){
+        v1 = '';
+        operator = '';
+        subtotal = 0;
+        console.log('clear')
+    }
+
+    v2 = '';
+    displayValue = null;
     displayUpdate();
+    document.getElementById("btn-clear").value = 'AC'; //activate btn AC
+
 }
